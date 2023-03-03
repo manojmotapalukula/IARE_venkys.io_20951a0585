5bf0be0afc9ca914d4a252e76630e2ae40f5faf7
Manoj Motapalukula <89991377+manojmotapalukula@users.noreply.github.com>
2023-03-03 21:18:52 +0530

    Update string_rev.py

--- subtring_reverse/string_rev.py
+++ subtring_reverse/string_rev.py
@@ -1,15 +1,35 @@
-def reverse_word(word):
-    if len(word) == 0 or len(word) == 1:
-        return word
-    else:
-        return reverse_word(word[1:]) + word[0]
+#Using multithreading in python
+import threading
+def reverse_string(string):
+    return string[::-1]
 
-def reverse_paragraph(paragraph):
+def reverse_words(paragraph):
     words = paragraph.split()
-    reversed_paragraph = " ".join(list(map(reverse_word, reversed(words))))
-    return reversed_paragraph
+    threads = []
+    reversed_words = [''] * len(words)
+    for i, word in enumerate(words):
+        t = threading.Thread(target=lambda i, word: reversed_words.__setitem__(i, reverse_string(word)), args=(i, word))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
 
-input_paragraph = input("Enter a paragraph: ")
-reversed_paragraph = reverse_paragraph(input_paragraph)
-print("Reversed paragraph:")
+    return ' '.join(reversed_words)
+
+paragraph = input()
+reversed_paragraph = reverse_words(paragraph)
 print(reversed_paragraph)
+
+
+#Using recursion in python
+def rev(w):
+    if len(w)==0:
+        return ""
+    else:
+        return w[-1]+rev(w[:-1])
+def rev_s(s):
+    word=s.split()
+    r_word=[rev(i) for i in word]
+    return " ".join(r_word)
+s=input("Enter a Paragraph:")
+print(rev_s(s))
